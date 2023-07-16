0543c9915b3e6002d62d2fc1a91bd1377ebabb4a
xleepy <ragux0@gmail.com>
2023-07-16 16:09:44 +0200

    Add test webview with random archive video

--- App.tsx
+++ App.tsx
@@ -5,29 +5,29 @@
  * @format
  */
 
-import React, {useEffect, useState} from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
   useColorScheme,
   View,
 } from 'react-native';
+import WebView from 'react-native-webview';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+  },
+});
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
-  const [segments, setSegments] = useState();
+  const [segments, setSegments] = useState<Record<string, any>[]>();
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
@@ -36,8 +36,15 @@ function App(): JSX.Element {
   useEffect(() => {
     fetch('https://bsu.drhx.ru/data/segments.json')
       .then(res => res.json())
-      .then(setSegments);
+      .then(result => {
+        const allVideos: Record<string, any>[] = Object.values(result);
+        setSegments(allVideos);
+      });
   }, []);
+
+  const [firstSegment] = segments ?? [];
+
+  const youtubeId = firstSegment?.youtube;
 
   return (
     <SafeAreaView style={backgroundStyle}>
@@ -45,16 +52,21 @@ function App(): JSX.Element {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Text>{JSON.stringify(segments, null, 2)}</Text>
-        </View>
-      </ScrollView>
+      <View style={styles.container}>
+        {youtubeId && (
+          <WebView
+            startInLoadingState
+            javaScriptEnabled
+            allowsFullscreenVideo
+            allowsInlineMediaPlayback
+            mediaPlaybackRequiresUserAction
+            onError={err => console.log('err', err)}
+            source={{
+              html: `<iframe width="100%" height="100%" src="https://www.youtube.com/embed/${youtubeId}" title="Mafia II #3 [03.11.19] (перезалив⁴)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,
+            }}
+          />
+        )}
+      </View>
     </SafeAreaView>
   );
 }

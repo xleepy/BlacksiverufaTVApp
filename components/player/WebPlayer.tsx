724ba822dc7058578f9c9ed093361c9b9a5de038
Radmir Gurkov <ragux0@gmail.com>
2023-08-13 23:46:30 +0200

    WIP tv support for players

--- components/player/WebPlayer.tsx
+++ components/player/WebPlayer.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useTVEventHandler } from 'react-native';
 import WebView from 'react-native-webview';
 
 const runFirst = `
 window.isNativeApp = true;
 true; // note: this is required, or you'll sometimes get silent failures
-`;
+  `;
 
 type Props = {
   name: string;
@@ -12,12 +13,19 @@ type Props = {
 };
 
 export const WebPlayer = ({ name, youtube }: Props) => {
+  const webViewRef = useRef<WebView>();
+  useTVEventHandler(event => {
+    console.log(event);
+  });
   return (
     <WebView
+      ref={webViewRef as any}
       injectedJavaScriptBeforeContentLoaded={runFirst}
+      injectedJavaScript="window.postMessage(document.body))"
       allowsFullscreenVideo
       thirdPartyCookiesEnabled
       accessibilityLabel={name}
+      onMessage={message => console.log(message)}
       source={{
         uri: `https://www.youtube.com/embed/${youtube}`,
       }}
